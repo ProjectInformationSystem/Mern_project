ef726d02c57ea85da7f43e7ad9f5ebdc07127c2e
Abhishek Kumar <abhishek.it.msit@gmail.com>
2022-11-21 10:28:19 +0000

    Create a Product API

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,18 @@
+const Product = require("../models/productModel")
+
+
+// Creating a Product - Admin 
+
+exports.createProduct = async(req,res,next)=>{
+
+   const product = await Product.create(req.body);
+
+   res.status(201).json({
+    success:true,
+    product
+   })
+}
+
 exports.getAllProducts = (req,res) =>{
     res.status(200).json({message:"Route is working fine"});
 }
